73c5a69a5b971c82805df1ca82685644bd345c0d
kresposs <email@marcosumma.com>
2021-04-20 10:49:06 +0200

    Initial commit

---
+++ bulkUsers_CiscoASA-VPN.py
@@ -0,0 +1,38 @@
+"""
+This script lets you create Anyconnect VPN users in bulk mode
+"""
+import sys
+import pyfiglet
+
+
+
+def main():
+    """
+    Main function that executes the script
+    """
+
+    banner = pyfiglet.figlet_format('CISCO ASA - Remote Access VPN users')
+    print(banner)
+
+    print("Please, choose group-policy to associate with the users: ")
+    groupPolicy = input()
+
+
+    with open("C:\\Users\\summam\\users.txt") as f:
+        usersList = f.read()
+
+    usersList = usersList.split("\n")
+
+    with open("C:\\Users\\summam\\password.txt") as f:
+        passwordsList = f.read()
+
+    passwordsList = passwordsList.split("\n")
+
+    for i in range(0, 51):
+        print(f"username {usersList[i]} password {passwordsList[i]} privilege 0")
+        print(f"username {usersList[i]} attributes")
+        print(f" vpn-group-policy {groupPolicy}\n service-type remote-access")
+
+
+if __name__ == "__main__":
+    main()
